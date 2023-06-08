c30fb2fe808148903833f1cef375c3ce5dd98066
mroberts66 <marober7.mr@gmail.com>
2023-06-07 22:23:12 -0700

    Add todo comment

--- src/ButtonExercise.jsx
+++ src/ButtonExercise.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import './App.css';
 
 function IndividualButton() {
+    // to do: use an array of colors. add/remove from array functionality, click cycling
     const [color, setColor] = useState('red');
     return <button className={color} onClick={() => setColor('blue')}>
         Button
